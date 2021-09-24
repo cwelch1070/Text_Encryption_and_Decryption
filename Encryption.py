6d36b02d57c376426b6236413ecf81f66b0fa4d7
cwelch1070 <90748206+cwelch1070@users.noreply.github.com>
2021-09-24 15:30:19 -0500

    Update Encryption.py
    Program now will decrypt the encrypted text. The decrypted text is printed out as 5 different outputs with one being the correct word the was originally encrypted.
    Issues:
    - Prints a random character before each decrypted word

--- Encryption.py
+++ Encryption.py
@@ -6,8 +6,10 @@ def encrypt():
     #Loops through the characters in the word and changed them for encryption
     for x in range(len(user1)):
         char = user1[x]
+        #This handles all upper case letters
         if(char.isupper()):
             cipher += chr((ord(char) + user2 - 65) % 26 + 65)
+        #This handles all lower case letters
         else:
             cipher += chr((ord(char) + user2 - 97) % 26 + 97)
     
@@ -17,10 +19,83 @@ def encrypt():
     sample = open('Crypto.txt', 'w')
     print(cipher, file = sample)
     sample.close()
+    
+    return cipher
 
+def decrypt():
+    cipher = encrypt()
+    num = [1, 2, 3, 4, 5]
+    decryption = ' '
+    size = len(cipher)
+    #Loops through the characters in the word and changed them for encryption
+    #LOOP 1
+    for x in range(len(cipher)):
+        char = cipher[x]
+        #This handles all upper case letters
+        if(char.isupper()):
+            decryption += chr((ord(char) - num[0])) #- 65) % 26 + 65)
+        #This handles all lower case letters
+        else:
+            decryption += chr((ord(char) - num[0] - 97) % 26 + 97)
+            
+    print(f"Decrypted String: {decryption}")
+    decryption = ' '
+            
+    #LOOP 2
+    for x in range(len(cipher)):
+        char = cipher[x]
+        #This handles all upper case letters
+        if(char.isupper()):
+            decryption += chr((ord(char) - num[1])) #- 65) % 26 + 65)
+        #This handles all lower case letters
+        else:
+            decryption += chr((ord(char) - num[1] - 97) % 26 + 97)
+            
+    print(f"Decrypted String: {decryption}")
+    decryption = ' '
+       
+    #LOOP 3
+    for x in range(len(cipher)):
+        char = cipher[x]
+        #This handles all upper case letters
+        if(char.isupper()):
+            decryption += chr((ord(char) - num[2])) #- 65) % 26 + 65)
+        #This handles all lower case letters
+        else:
+            decryption += chr((ord(char) - num[2] - 97) % 26 + 97)
+            
+    print(f"Decrypted String: {decryption}")
+    decryption = ' '
+          
+    #LOOP 4       
+    for x in range(len(cipher)):
+        char = cipher[x]
+        #This handles all upper case letters
+        if(char.isupper()):
+            decryption += chr((ord(char) - num[3])) #- 65) % 26 + 65)
+        #This handles all lower case letters
+        else:
+            decryption += chr((ord(char) - num[3] - 97) % 26 + 97)
+    
+    print(f"Decrypted String: {decryption}")
+    decryption = ' '   
+         
+    #LOOP 5
+    for x in range(len(cipher)):
+        char = cipher[x]
+        #This handles all upper case letters
+        if(char.isupper()):
+            decryption += chr((ord(char) - num[4])) #- 65) % 26 + 65)
+        #This handles all lower case letters
+        else:
+            decryption += chr((ord(char) - num[4] - 97) % 26 + 97)
+    
+    print(f"Decrypted String: {decryption}")
+    
 #Gets user input for stirng to encrypt and how far to shift 
 user1 = input("Enter a line of text to encrypt: ")
 user2 = int(input("Enter the number of shifts needed for this type of encryption: "))
 
 #Function call
 encrypt()
+decrypt()
